a23c0e679e6eb9a08d48f1526017983cab3d28f7
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2025-01-28 21:00:22 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pool-guy",
-    version="0.1.0",
+    version="0.1.1",
     author="s4w3d0ff",
     author_email="",
     description="A Twitch bot framework with event subscription and alert handling capabilities",
