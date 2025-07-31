f45c02a649edeba058e317fb080a61947bf361ab
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2025-07-31 18:00:53 +0000

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .twitch import TwitchBot, CommandBot, command, rate_limit
-from .twitchws import TwitchWebsocket, Alert
-from .twitchapi import TwitchApi
-from .oauth import TokenHandler
-from .storage import StorageFactory
-from .webserver import WebServer, route, websocket
